b5aa8e6609117d3744a51aed4fe31a3a1b07b727
Tommy Janszen <tommy.janszen@gmail.com>
2015-02-12 10:40:44 -0800

    Update minor changes to layout

--- client/main.js
+++ client/main.js
@@ -2,11 +2,6 @@
 
 $(document).ready(clickNsa);
 
-// function init() {
-//   // $('#get-weather').click(clickGetCams);
-//   // $('#nsa').click(clickNsa);
-// }
-
 function clickNsa() {
   var options = {enableHighAccuracy: true, timeout: 5000, maximumAge: 0};
   navigator.geolocation.getCurrentPosition(success, error, options)
@@ -16,7 +11,9 @@ function success(pos) {
   var urlInfo = 'http://api.wunderground.com/api/5ac2a3bc4dece267/forecast10day/q/' + pos.coords.latitude +',' +   pos.coords.longitude + '.json';
   var urlCond = 'http://api.wunderground.com/api/5ac2a3bc4dece267/conditions/q/' + pos.coords.latitude +',' +   pos.coords.longitude + '.json';
   var urlCity = 'http://api.wunderground.com/api/5ac2a3bc4dece267/geolookup/q/' + pos.coords.latitude +',' +   pos.coords.longitude + '.json';
+  var urlHourly = 'http://api.wunderground.com/api/5ac2a3bc4dece267/hourly10day/q/' + pos.coords.latitude +',' +   pos.coords.longitude + '.json';
   getForecastData(urlInfo);
+  getHourlyData(urlHourly);
   getCondData(urlCond);
   getCityData(urlCity);
 }
@@ -24,11 +21,6 @@ function success(pos) {
 function error(err) {
   console.log('could not find your position', err)
 }
-
-// function clickGetCams() {
-//   var zipCode = $('#zip-code').val();
-//   var url = 'http://api.wunderground.com/api/5ac2a3bc4dece267/forecast10day/q/' + zipCode + '.json';
-// }
 
 function getForecastData(urlInfo) {
   var dates = [];
@@ -42,7 +34,6 @@ function getForecastData(urlInfo) {
 
   $.getJSON(urlInfo, function(response) {
     response.forecast.simpleforecast.forecastday.forEach(function(a){
-      console.log(response.forecast.simpleforecast.forecastday)
       dates.push(a['date']['day']);
       dayShorts.push(a['date']['weekday']);
       tempsHighF.push(a['high']['fahrenheit']);
@@ -52,7 +43,29 @@ function getForecastData(urlInfo) {
       conditions.push(a['conditions']);
       monthShort.push(a['date']['monthname_short']);
     })
-    addForecastData(dates, dayShorts, tempsHighF, tempsLowF, conds, icons, conditions);
+    addForecastData(dates, dayShorts, tempsHighF, tempsLowF, conds, icons, conditions, monthShort);
+    })
+}
+
+function getHourlyData(urlHourly){
+  var hour = "";
+  var temp = "";
+  var icon = "";
+  var hours = [];
+  var hourlys = [];
+
+  $.getJSON(urlHourly, function(response) {
+    response.hourly_forecast.forEach(function(a){
+      hour = a['FCTTIME']['hour'];
+      hours.push(hour)
+      temp = a['temp']['english'];
+      hours.push(temp)
+      icon = a['icon_url'];
+      hours.push(icon)
+      hourlys.push(hours);
+      hours = [];
+    })
+    addHourlyData(hourlys);
     })
 }
 
@@ -67,19 +80,22 @@ function getCondData(urlCond){
 
 function getCityData(urlCity){
 var city = ""
+var state = ""
   $.getJSON(urlCity, function(response) {
     city = response.location['city'];
-    addCityData(city);
+    state = response.location['state'];
+    addCityData(city, state);
   })
 }
 
-function addForecastData(dates, dayShorts, tempsHighF, tempsLowF, conds, icons, conditions){
+function addForecastData(dates, dayShorts, tempsHighF, tempsLowF, conds, icons, conditions, monthShort){
   var count = 1;
   for(var i = 0; i < 11; i++){
     $('#day' + count).text(dayShorts[count-1]);
     $('#icon' + count).css('background-image', 'url(' + icons[count-1] + ')');
     $('#tHigh' + count).text(tempsHighF[count-1]);
     $('#tLow' + count).text(tempsLowF[count-1]);
+    $('#day1').text('Today')
 
     count+= 1;
     if(count === 11){
@@ -87,13 +103,31 @@ function addForecastData(dates, dayShorts, tempsHighF, tempsLowF, conds, icons, 
     }
   }
   $('#cond').text(conditions[0])
+  $('#monthDay').text(monthShort[0] + " " + dates[0]);
 }
 
 function addCondData(feelsLike){
-  console.log(feelsLike);
-  $('#currentTemp').text(feelsLike);
+  $('#currentTemp').text(Math.round(feelsLike));
 }
 
-function addCityData(city){
-  $('#city').text(city);
+function addCityData(city, state){
+  $('#city').text(city + ", " + state);
+}
+
+function addHourlyData(hourlys){
+// var hourlyHour = "";
+// var hourlyTemp = "";
+// var hourlyIcon = "";
+// var $hourlyHour = $('.hourlyHour');
+// var $hourlyTemp = $('.hourlyTemp');
+// var $hourlyIcon = $('.hourlyIcon');
+// var count = 0;
+//
+//   for(var i = 0; i < 24; i++){
+//     $hourlyHour.append($('<div>').text(hourlys[i][0] + ":00 " + hourlys[i][1]));
+//     debugger;
+//     if(hourlys[i][0] === "23"){
+//     return;
+//     }
+//   }
 }
